19201aa739a1b55cf7a4bfe51c7ad25019dbfa30
Amir Adel <amir.elbashary@gmail.com>
2019-04-19 18:48:37 +0200

    Admin can generate new app token

--- app/controllers/api/v1/admin/app_tokens_controller.rb
+++ app/controllers/api/v1/admin/app_tokens_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::Admin::AppTokensController < Api::V1::Admin::BaseAdminController
+  before_action :authenticate_admin
+
+  def create
+    AppToken.destroy_all
+    @app_token = AppToken.create(title: 'OnShop')
+
+    render json: { status: 'success', new_token: @app_token.token }
+  end
 end
