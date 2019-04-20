eb6c170be95ff2654a6e1ad48593aad0e642ab95
Amir Adel <amir.elbashary@gmail.com>
2019-04-20 03:49:34 +0200

    Add index for categories parent_id

---
+++ db/migrate/20190420014839_add_index_for_categories_parents.rb
@@ -0,0 +1,5 @@
+class AddIndexForCategoriesParents < ActiveRecord::Migration[5.2]
+  def change
+    add_index :categories, :parent_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_165849) do
+ActiveRecord::Schema.define(version: 2019_04_20_014839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_04_19_165849) do
     t.integer "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
 end
