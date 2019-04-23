df30f27a774aaca57373cafeef6d85f7296e2039
Amir Adel <amir.elbashary@gmail.com>
2019-04-23 16:29:15 +0200

    Fix issue where a quoted name would case SQL issues (Quote should not be on categories)

--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :category do
-    name { Faker::Name.unique.name.downcase.strip.squeeze }
+    name { Faker::Name.unique.name.downcase.strip.squeeze.parameterize }
     image { Faker::Avatar.image }
 
     factory :sub_category do
