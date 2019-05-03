75bf9cc2d727358fa26927c806ca7211bc5c1729
Amir Adel <amir.elbashary@gmail.com>
2019-05-03 13:16:12 +0200

    Implement cors-rack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::API
   check_authorization
   before_action :set_locale
 
+  private
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
