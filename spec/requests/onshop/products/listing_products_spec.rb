1515ac5f4e97479eb8920ce80a9aebc458657a38
Amir Adel <amir.elbashary@gmail.com>
2019-06-10 20:13:58 +0200

    Products API auto detecting root and parent categories

--- spec/requests/onshop/products/listing_products_spec.rb
+++ spec/requests/onshop/products/listing_products_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe 'Listing products', type: :request do
   before do
     @app_token = create(:app_token)
     @headers = { 'X-APP-Token' => @app_token.token }
-    @category1 = create(:category)
-    @category2 = create(:category)
-    @cat1_product1 = create(:product, category: @category1)
-    @cat1_product1 = create(:product, category: @category1)
-    @cat2_product1 = create(:product, category: @category2)
-    @cat2_product2 = create(:product, category: @category2)
+    @category = create(:category)
+    @sub_category1 = create(:category, parent: @category)
+    @sub_category2 = create(:category, parent: @category)
+    @sub1_product1 = create(:product, category: @sub_category1)
+    @sub1_product2 = create(:product, category: @sub_category1)
+    @sub2_product1 = create(:product, category: @sub_category2)
+    @sub2_product2 = create(:product, category: @sub_category2)
     @other_product = create(:product)
   end
 
@@ -26,9 +27,21 @@ RSpec.describe 'Listing products', type: :request do
   end
 
   describe 'when provding category ID' do
-    context 'if it\'s valid category id' do
-      it 'should return this category products' do
-        params = { category_id: @category1.id }
+    context 'if it\'s valid parent category id' do
+      it 'should return this category\'s sub categories products' do
+        params = { category_id: @category.id }
+
+        get '/v1/onshop/products', headers: @headers, params: params
+        response_body = JSON.parse(response.body)
+
+        expect(response.code).to eq('200')
+        expect(response_body.count).to eq(4)
+      end
+    end
+
+    context 'if it\'s valid sub category id' do
+      it 'should return this sub category products' do
+        params = { category_id: @sub_category1.id }
 
         get '/v1/onshop/products', headers: @headers, params: params
         response_body = JSON.parse(response.body)
