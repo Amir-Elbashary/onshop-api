7d87d3da3267604bea68e4c044c97bf42cf7c050
Amir Adel <amir.elbashary@gmail.com>
2019-06-27 17:11:38 +0200

    Add simple statistics API

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :admin do
         resources :admins
+        resources :statistics, only: :show
         resources :merchants
         resources :users
         resources :app_tokens, only: :create
