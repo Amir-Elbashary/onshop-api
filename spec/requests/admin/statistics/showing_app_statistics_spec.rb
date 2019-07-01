cf2c99e8ac7b89b67595caa995970552e14db5f8
Amir Adel <amir.elbashary@gmail.com>
2019-07-01 21:13:00 +0200

    Add RSpec for statistics API

---
+++ spec/requests/admin/statistics/showing_app_statistics_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Getting current app statistics by an admin', type: :request do
+  before do
+    @app_token = create(:app_token)
+    @admin = create(:admin)
+    @headers = { 'X-APP-Token' => @app_token.token, 'X-User-Token' => @admin.authentication_token }
+  end
+
+  context 'with valid admin token' do
+    it 'should show app statistics' do
+      headers = { 'X-APP-Token' => @app_token.token, 'X-User-Token' => @admin.authentication_token }
+      get "/v1/admin/statistics/id", headers: headers
+
+      response_body = JSON.parse(response.body)
+
+      expect(response.code).to eq('200')
+      expect(response_body.size).to eq(5)
+    end
+  end
+
+  context 'with invalid admin token' do
+    it 'should return unauthorized' do
+      headers = { 'X-APP-Token' => @app_token.token, 'X-User-Token' => 'invalid token' }
+      get "/v1/admin/statistics/id", headers: headers
+
+      expect(response.code).to eq('401')
+    end
+  end
+end
