a53607a08bff1efd8a3032833525748b153c7c97
Amir Adel <amir.elbashary@gmail.com>
2019-07-22 10:10:51 +0200

    Replace squeeze with squish

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-    name { Faker::Name.unique.name.downcase.strip.squeeze }
+    name { Faker::Name.unique.name.downcase.strip.squish }
     description { Faker::Lorem.paragraph(8) }
     image { Faker::Avatar.image }
     merchant
