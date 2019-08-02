e07ca81c34365ff570ca7fb29ec8e81d48231b5f
Amir Adel <amir.elbashary@gmail.com>
2019-08-02 17:40:22 +0200

    Wishlist returns products variants

--- spec/requests/user/favourites/listing_favourite_products_spec.rb
+++ spec/requests/user/favourites/listing_favourite_products_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe 'Getting user favourite products', type: :request do
     @user = create(:user_with_logins)
     @headers = { 'X-APP-Token' => @app_token.token, 'X-User-Token' => @user.logins.first.token }
     @product1 = create(:product)
+    @product1_variant1 = create(:variant, product: @product1)
+    @product1_variant2 = create(:variant, product: @product1)
     @product2 = create(:product)
     Favourite.create(user: @user, favourited: @product1)
     Favourite.create(user: @user, favourited: @product2)
@@ -20,6 +22,9 @@ RSpec.describe 'Getting user favourite products', type: :request do
 
       expect(response.code).to eq('200')
       expect(response_body.count).to eq(2)
+      expect(response_body[0]['variants'].count).to eq(2)
+      expect(response_body[0]['variants'][0]['id']).to eq(@product1_variant1.id)
+      expect(response_body[0]['variants'][1]['id']).to eq(@product1_variant2.id)
     end
   end
 
